e2b4999018f5d2ffb4c38ee61c73e9b32a25c3db
Emma Labbé <adrilabbelol@gmail.com>
2021-06-09 15:16:31 -0400

    Fix count on non persistent background

--- Deadname Remover Extension/Resources/background.js
+++ Deadname Remover Extension/Resources/background.js
@@ -1,19 +1,23 @@
-var timesDeadnamed = {};
-
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     
     console.log("Received request: ", request);
     
-    if (request == "names") {
-        browser.runtime.sendNativeMessage("Deadname Remover", {}, function(response) {
-            sendResponse(response);
-        });
-        return true;
-    } else if (request.deadnamed == undefined) {
-        var res = { deadnamed: timesDeadnamed[request.tabID] };
-        sendResponse(res);
-    } else {
-        timesDeadnamed[sender.tab.id] = request.deadnamed;
-        sendResponse({ ok: 2 });
-    }
+    browser.storage.local.get((item) => {
+        var timesDeadnamed = item;
+        
+        if (request == "names") {
+            browser.runtime.sendNativeMessage("Deadname Remover", {}, function(response) {
+                sendResponse(response);
+            });
+        } else if (request.deadnamed == undefined) {
+            var res = { deadnamed: timesDeadnamed[request.tabID] };
+            sendResponse(res);
+        } else {
+            timesDeadnamed[sender.tab.id] = request.deadnamed;
+            browser.storage.local.set(timesDeadnamed);
+            sendResponse({ ok: 2 });
+        }
+    });
+    
+    return true;
 });
